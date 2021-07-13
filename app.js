203d150d825a67f5a50f43e5d2cfb5aa2a010d28
Duhan Bayrak <duhanbayrak6@gmail.com>
2021-07-13 15:46:21 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+
+var startBtn = document.getElementById("startBtn");
+var city = document.getElementById("city");
+const element = document.querySelector("#svg-turkiye-haritasi");
+const mapPath = document.querySelectorAll("#svg-turkiye-haritasi path");
+const trueScore = document.getElementById("dogruSayisi");
+const counter = document.getElementById("counter");
+
+var cities = ["Adana", "Adıyaman", "Afyonkarahisar", "Ağrı", "Amasya", "Ankara", "Antalya", "Artvin", "Aydın", "Balıkesir", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Isparta", "Mersin", "İstanbul", "İzmir", "Kars", "Kastamonu", "Kayseri", "Kırklareli", "Kırşehir", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Uşak", "Van", "Yozgat", "Zonguldak", "Aksaray", "Bayburt", "Karaman", "Kırıkkale", "Batman", "Şırnak", "Bartın", "Ardahan", "Iğdır", "Yalova", "Karabük", "Kilis", "Osmaniye", "Düzce"];
+
+var randomArray = cities.sort(() => Math.random() - 0.5);
+
+var index = 0;
+
+function changeCity() {
+    index++;
+    city.textContent = `${randomArray[index]}`;
+}
+
+function startGame() {
+    
+    city.textContent = `${randomArray[index]}`;
+    
+    var dogru = 0, Puan, remaining = 15;
+    
+    counter.textContent = remaining;
+
+    element.addEventListener("click", (event) => {
+        
+
+        if (event.target.tagName === "path") {
+            const parent = event.target.parentNode;
+
+
+            if (parent.getAttribute("data-iladi") === randomArray[index]) {
+                changeCity();
+                event.target.style.fill = "#57837B";
+                dogru++;
+                remaining += 2;
+                trueScore.innerText = `${dogru}`;
+                
+            }
+            else {
+                remaining--;
+                counter.textContent = remaining;
+                
+            }
+
+            if (dogru >= 81) {
+                counter.textContent = "Tebrikler! Bütün Şehirleri Doğru Bildiniz.";
+                element.style.display = "none";
+            }
+            puan = (dogru * 10);
+
+            if (puan < 0) {
+                puan = 0;
+
+            }
+        }
+        if (remaining <= 0) {
+            counter.innerHTML = "Puan: " + puan;
+            dogru = 0;
+            remaining = 10;
+            trueScore.innerText = `${dogru}`;
+            startBtn.textContent = "Tekrar Başla";
+            element.style.display = "none";
+
+            startBtn.onclick = function() {
+                element.style.display = "block";
+                remaining = 10;
+
+                mapPath.forEach((el) => {
+                    el.attributeStyleMap.clear();
+                    
+                });
+
+                counter.innerHTML = remaining;
+                changeCity();
+
+            };
+        }
+    });
+};
+
+
+
+
+
+
+
